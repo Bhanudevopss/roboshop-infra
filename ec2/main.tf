7a60f3c6f3a65423981e3307dda0bd586becc883
Bhanu Pratap Reddy <Bhanu.devopss@gmail.com>
2023-03-03 21:17:12 +0530

    Update roboshop-infra/

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,17 +1,17 @@
 data "aws_caller_identity" "current" {}
 
 data "aws_ami" "ami" {
-  most_recent      = true
-  name_regex       = "devops-practice-with-ansible"
-  owners           = [data.aws_caller_identity.current.account_id]
+  most_recent = true
+  name_regex  = "devops-practice-with-ansible"
+  owners      = [data.aws_caller_identity.current.account_id]
 }
 
 
 resource "aws_instance" "ec2" {
-  ami                    = "ami-0a017d8ceb274537d"
+  ami                    = data.aws_ami.ami.image_id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.sg.id]
-  tags                   = {
+  tags = {
     Name = var.component
   }
 }
@@ -20,8 +20,8 @@ resource "null_resource" "provisioner" {
   provisioner "remote-exec" {
 
     connection {
-      host = aws_instance.ec2.public_ip
-      user = "centos"
+      host     = aws_instance.ec2.public_ip
+      user     = "centos"
       password = "DevOps321"
     }
 
@@ -38,18 +38,18 @@ resource "aws_security_group" "sg" {
   description = "Allow TLS inbound traffic"
 
   ingress {
-    description      = "ALL"
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "ALL"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
