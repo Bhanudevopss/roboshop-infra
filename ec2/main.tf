2aa80b35c622c98bc385b6617819d85d8e7c7422
Bhanu Pratap Reddy <Bhanu.devopss@gmail.com>
2023-03-07 13:56:19 +0530

    Update roboshop-infra/

--- ec2/main.tf
+++ ec2/main.tf
@@ -18,7 +18,7 @@ resource "null_resource" "provisioner" {
     }
 
     inline = [
-      "ansible-pull -i localhost, -U https://github.com/Bhanudevopss/roboshop-ansible roboshop.yml -e role_name=${var.component}"
+      "ansible-pull -i localhost, -U https://github.com/Bhanudevopss/roboshop-ansible roboshop.yml -e role_name=${var.component} -e env=${env}"
     ]
 
   }
