4c622c8275c0d9d3a81e6369898311601b89bd59
Bhanu Pratap Reddy <Bhanu.devopss@gmail.com>
2023-05-16 12:58:37 +0530

    Update roboshop-infra/

--- main.tf
+++ main.tf
@@ -11,7 +11,6 @@ module "vpc" {
   private_subnets = each.value["private_subnets"]
 }
 
-/*
 module "docdb" {
   source = "git::https://github.com/Bhanudevopss/tf-module-docdb.git"
   env    = var.env
@@ -83,6 +82,7 @@ module "rabbitmq" {
 
 }
 
+/*
 module "alb" {
   source = "git::https://github.com/Bhanudevopss/tf-module-alb.git"
   env    = var.env
@@ -170,7 +170,6 @@ resource "null_resource" "load-gen" {
     ]
   }
 }
-*/
 
 module "minikube" {
   source = "github.com/scholzj/terraform-aws-minikube"
@@ -202,8 +201,8 @@ output "MINIKUBE_SERVER" {
 output "KUBE_CONFIG" {
   value = "scp centos@${module.minikube.public_ip}:/home/centos/kubeconfig ~/.kube/config"
 }
-
 */
+
 module "eks" {
 source             = "github.com/r-devops/tf-module-eks"
 ENV                = var.env
